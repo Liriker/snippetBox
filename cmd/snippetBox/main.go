3cb3ba69a73450e714328ebf575ab0690fd8593e
Likerchik <likerchik.work@mail.ru>
2023-01-15 18:56:56 +0300

    Expanded the pool of handlers and relevant URLs

--- cmd/snippetBox/main.go
+++ cmd/snippetBox/main.go
@@ -5,22 +5,37 @@ import (
 	"net/http"
 )
 
-// home - функция обработчик, сейчас имитирует ответ от пользователя
+// home - обработчик главной страницы
 func home(w http.ResponseWriter, r *http.Request) {
+	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
+	// функция http.NotFound() для возвращения клиенту ошибки 404.
+	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
+	// продолжит работу и выведет сообщение "Привет из SnippetBox" как ни в чем не бывало.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Привет из SnippetBox"))
 }
 
+// showSnippet - обработчик для отображения содержимого заметки
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки"))
+}
+
+// createSnippet - обработчик создания новой заметки
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма создания заметки"))
+}
+
 func main() {
-	// http.NewServeMux - функция инициализации нового рутера
-	// функция home регистрируется как обработчик URL шаблона "/"
+	// Регистрируем обработчики и соответствующие url- шаблоны в маршрутизаторе
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// http.ListenAndServe() - функция для запуска нового веб-сервера
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания
-	//(в данном случае это "localhost:4000") и созданный рутер.
-	// log.Fatal() используется  для логирования ошибок.
-	//Любая ошибка, возвращаемая ListenAndServe() всегда non-nil.
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
