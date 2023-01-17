41b70ad4bd9cc19a0e1d41e537e26f2bd9ffb4bf
Likerchik <likerchik.work@mail.ru>
2023-01-17 16:24:21 +0300

    add handler for static files in main function

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,16 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Инициалзируем FileServer. Он будет обрабатывать
+	// HTTP-запросык статическим файлам в папке "./ui/static"
+	// Путь является относительным корневой папки проекта
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Handle регистрирует обработчик
+	// Он обрабатывает запросы к статическим файлам в папке" "./ui/static"
+	// StripPrefix убирает префикс "/static"прежде чем запрос достигнет http.fileServer
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
